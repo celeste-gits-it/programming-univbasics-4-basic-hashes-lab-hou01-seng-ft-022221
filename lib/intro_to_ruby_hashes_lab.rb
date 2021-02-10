ed427636bcefa726aff72003e84dded79b3a9554
Celeste Carter <celesteanell@gmail.com>
2021-02-10 16:50:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,11 @@ def new_hash
 end
 
 def my_hash
-  my_hash = {pizza: "pineapple"}
+  my_hash = {key: "value"}
 end
 
 def pioneer
-  {name: 'Grace Hopper'}
+  pioneer = {name: 'Grace Hopper'}
 end
 
 
